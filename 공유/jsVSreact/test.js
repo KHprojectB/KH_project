db0dccf9aa88399744136c4e350f4248fa7e8e9b
김영훈 <joseph0926@naver.com>
2022-08-31 05:18:49 +0900

    jsVSreact

--- 공유/jsVSreact/test.js
+++ 공유/jsVSreact/test.js
@@ -1,11 +1,2 @@
-const p = document.querySelector(".p")
-const openBtn = document.querySelector(".open")
-const closeBtn = document.querySelector(".close")
 
-openBtn.addEventListener("click", () => {
-  p.classList.add("show")
-})
-closeBtn.addEventListener("click", () => {
-  p.classList.remove("show")
-})
 
--- 공유/jsVSreact/test/src/components/test/Test.js
+++ 공유/jsVSreact/test/src/components/test/Test.js
@@ -4,20 +4,13 @@ import "./Test.css"
 
 const Test = () => {
 
-  const [toggle, setToggle] = useState(false);
-  const openHandler = () => {
-    setToggle(true);
-  }
-  const closeHandler = () => {
-    setToggle(false);
-  }
 
   return (
     <div className="test">
-      {toggle && <p className="p">
+      <p className="p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae vel dolorem numquam, a earum impedit in minima dolor voluptates
         expedita deserunt magni quaerat nemo quis illo! Dolore nemo error asperiores!
-      </p>}
+      </p>
       <div className="btn-con">
         <button className="open" onClick={openHandler}>open</button>
         <button className="close" onClick={closeHandler}>close</button>
