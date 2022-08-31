d5d4e05b03501a7684122a3f5d6c807b95c21d0d
김영훈 <joseph0926@naver.com>
2022-08-31 12:31:26 +0900

    admin

--- 공유/jsVSreact/test/src/components/test/Test.js
+++ 공유/jsVSreact/test/src/components/test/Test.js
@@ -5,17 +5,27 @@ import Modal from "../modal/Modal";
 
 const Test = (props) => {
 
+  const [state, setState] = useState(false);
+  
+  const openHandler = () => {
+    setState(true);
+  }
+  const closeHandler = () => {
+    setState(false);
+  }
+
+
   return (
       <div className="test">
-        <p className="p">
+        {state && <p className="p">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae vel dolorem numquam, a earum impedit in minima dolor voluptates
           expedita deserunt magni quaerat nemo quis illo! Dolore nemo error asperiores!
-        </p>
+        </p>}
         <div className="btn-con">
-          <button className="open">
+          <button className="open" onClick={openHandler}>
             open
           </button>
-          <button className="close">
+          <button className="close" onClick={closeHandler}>
             close
           </button>
         </div>
